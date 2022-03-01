59d8ffca2e52d15846ff164dbf32f3e5cac71ec7
JaavLex <alexandre.javet@hotmail.com>
2022-03-01 12:36:54 +0100

    [style] Beautified server.ts

--- server.ts
+++ server.ts
@@ -1,21 +1,23 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import { ApolloServer } from 'apollo-server'
-import { PluginDefinition } from 'apollo-server-core'
+import { Prisma, PrismaClient } from '@prisma/client';
+import { ApolloServer } from 'apollo-server';
+import { PluginDefinition } from 'apollo-server-core';
 
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
-import { schema } from './nexus/schema'
+import { schema } from './nexus/schema';
 
 type TestInjections = {
-  onQuery ?: (q: Prisma.QueryEvent) => void
-}
+	onQuery?: (q: Prisma.QueryEvent) => void;
+};
 
-export function makeServer(config: BackendConfig,
-                           { onQuery }: TestInjections = {}) : ApolloServer {
-  const onQueryParams: Prisma.PrismaClientOptions = {}
-  if (onQuery) {
-    onQueryParams.log = [{ level: "query", emit: "event" }]
-  }
+export function makeServer(
+	config: BackendConfig,
+	{ onQuery }: TestInjections = {}
+): ApolloServer {
+	const onQueryParams: Prisma.PrismaClientOptions = {};
+	if (onQuery) {
+		onQueryParams.log = [{ level: 'query', emit: 'event' }];
+	}
 
 	// Found on https://github.com/prisma/prisma/issues/6088, and we really want Unix Sockets for security.
 	const unixDomainSocketParams = {
@@ -24,43 +26,41 @@ export function makeServer(config: BackendConfig,
 		},
 	} as ConstructorParameters<typeof PrismaClient>[0];
 
-  const prisma = new PrismaClient({
+	const prisma = new PrismaClient({
 		datasources: { db: { url: config.LHD_DB_URL } },
 		...unixDomainSocketParams,
 		...onQueryParams,
 	});
 
-  if (onQuery) {
-    (prisma as any).$on("query", onQuery)
-  }
+	if (onQuery) {
+		(prisma as any).$on('query', onQuery);
+	}
 
-  return new ApolloServer({
-    context: () => ({ prisma }),
-    schema,
-    plugins: [
-      onServerStop(() => prisma.$disconnect())
-    ]
-  })
+	return new ApolloServer({
+		context: () => ({ prisma }),
+		schema,
+		plugins: [onServerStop(() => prisma.$disconnect())],
+	});
 }
 
 type BackendConfig = {
-  LHD_DB_URL: string
-}
+	LHD_DB_URL: string;
+};
 
-export function configFromDotEnv() : BackendConfig {
-  dotenv.config()
-  return process.env as BackendConfig
+export function configFromDotEnv(): BackendConfig {
+	dotenv.config();
+	return process.env as BackendConfig;
 }
 
 /**
  * An arbitrary stop-time callback, packaged as an ApolloServer plugin.
  */
 function onServerStop(cb: () => Promise<void>): PluginDefinition {
-  return {
-    async serverWillStart() {
-      return {
-        serverWillStop: cb
-      }
-    }
-  }
+	return {
+		async serverWillStart() {
+			return {
+				serverWillStop: cb,
+			};
+		},
+	};
 }
