e6bb9112d9d309655affde51996ade9089e83f01
JaavLex <alexandre.javet@hotmail.com>
2022-05-09 16:09:13 +0200

    [fix] Don't import `*`
    Typescript doesn't like that for whatever reason I don't care to investigate

--- tests/testlib/graphql.ts
+++ tests/testlib/graphql.ts
@@ -5,67 +5,70 @@
  * GraphQL can be queried with a simple REST-style API.
  */
 
-import fetch from 'node-fetch'
-import * as debug_ from 'debug'
+import fetch from 'node-fetch';
+import { debug as debug_ } from 'debug';
 import {
-  makeServer,
-  configFromDotEnv  // Hermetic tests for a brown-field DB are hard mmkay
-} from '../../server'
+	makeServer,
+	configFromDotEnv, // Hermetic tests for a brown-field DB are hard mmkay
+} from '../../server';
 
-import { HookFunction } from 'mocha'
-import { Prisma } from '@prisma/client'
+import { HookFunction } from 'mocha';
+import { Prisma } from '@prisma/client';
 
-const debug = debug_('lhd-tests:graphql')
+const debug = debug_('lhd-tests:graphql');
 
 export interface GraphQLClient<TRecord> {
-  query(query: string) : Promise< Array<TRecord> >
+	query(query: string): Promise<Array<TRecord>>;
 }
 
-export function graphqlClient<TRecord>(port: number) : GraphQLClient<TRecord> {
-  return {
-    async query(query: string) : Promise< Array<TRecord> > {
-      const fetched = await fetch(`http://localhost:${port}/graphql`, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query })
-      })
-      const json = await fetched.json()
-      debug(json)
-      for (const k in json.data) {
-        // In fact, we expect json.data to contain exactly one key
-        // (e.g. “rooms” for a `{ rooms { } }` query):
-        return json.data[k]
-      }
-      return []  // Not reached
-    }
-  }
+export function graphqlClient<TRecord>(port: number): GraphQLClient<TRecord> {
+	return {
+		async query(query: string): Promise<Array<TRecord>> {
+			const fetched = await fetch(`http://localhost:${port}/graphql`, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ query }),
+			});
+			const json = await fetched.json();
+			debug(json);
+			for (const k in json.data) {
+				// In fact, we expect json.data to contain exactly one key
+				// (e.g. “rooms” for a `{ rooms { } }` query):
+				return json.data[k];
+			}
+			return []; // Not reached
+		},
+	};
 }
 
-export function asGraphQL (whereClause : any) {
-  return JSON.stringify(whereClause).replace(/"([^"]*)":/g, (_match, $1) => $1+ ": ")
+export function asGraphQL(whereClause: any) {
+	return JSON.stringify(whereClause).replace(
+		/"([^"]*)":/g,
+		(_match, $1) => $1 + ': '
+	);
 }
 
 export function useTestServer<TRecord>(opts: {
-  before: HookFunction,
-  after: HookFunction,
-  onQuery?: (q: Prisma.QueryEvent) => void }
-) : () => GraphQLClient<TRecord> {
-  let port: number
-  let server: ReturnType<typeof makeServer>
+	before: HookFunction;
+	after: HookFunction;
+	onQuery?: (q: Prisma.QueryEvent) => void;
+}): () => GraphQLClient<TRecord> {
+	let port: number;
+	let server: ReturnType<typeof makeServer>;
 
-  let makeServerOpts = opts.onQuery ? { onQuery : opts.onQuery } : {}
+	let makeServerOpts = opts.onQuery ? { onQuery: opts.onQuery } : {};
 
-  opts.before(async () => {
-    server = makeServer(configFromDotEnv(), makeServerOpts)
-    const serverInfo = await server.listen(0)
-    port = serverInfo.port as number
-    console.log(`Test server listening on port ${port}`)
-  })
-  opts.after(async () => {
-    await server.stop()
-  })
-  return () => graphqlClient(port)
+	opts.before(async () => {
+		server = makeServer(configFromDotEnv(), makeServerOpts);
+		const serverInfo = await server.listen(0);
+		port = serverInfo.port as number;
+		console.log(`Test server listening on port ${port}`);
+	});
+	opts.after(async () => {
+		await server.stop();
+	});
+	return () => graphqlClient(port);
 }
