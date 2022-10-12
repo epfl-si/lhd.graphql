b43c107e95141409e545b34b60bf231905c81bd0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-12 17:17:39 +0200

    [workaround] Lack of IPv6 support in Docker

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import { makeServer, configFromDotEnv } from './server';
+import * as dns from 'dns';
+// Otherwise localhost is [::1], but unfortunately docker doesn't believe in ipv6
+dns.setDefaultResultOrder('ipv4first');
 
 const start = async () => {
 	const server = await makeServer(configFromDotEnv());
