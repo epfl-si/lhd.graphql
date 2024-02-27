bfda420da30a76fbe86e8bf187a56c6835ac7073
Rosa Maggi <rosa.maggi@epfl.ch>
2024-02-27 13:03:05 +0100

    [Feature] Add hazardCategory Nexus Struct

---
+++ schema/hazards/hazardCategory.ts
@@ -0,0 +1,18 @@
+import {booleanArg, extendType, inputObjectType, intArg, list, objectType, stringArg} from 'nexus';
+import { hazard_category } from 'nexus-prisma';
+
+export const HazardCategoryStruct = objectType({
+	name: hazard_category.$name,
+	description: `The list of hazards categories.`,
+
+	definition(t) {
+		t.nonNull.field(hazard_category.hazard_category_name);
+	},
+});
+
+/*export const HazardCategoryQuery = extendType({
+	type: 'Query',
+	definition(t) {
+		t.crud.hazardCategories({	filtering: true });
+	},
+});*/
