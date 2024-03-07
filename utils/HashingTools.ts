ae098890848b61f2dd59a47313384a866ec0462f
Rosa Maggi <rosa.maggi@epfl.ch>
2024-03-07 17:44:46 +0100

    [Feature] add encryption utility class

---
+++ utils/HashingTools.ts
@@ -0,0 +1,17 @@
+const CryptoJS = require("crypto-js");
+
+export function generateSalt(): string {
+	return CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex);
+}
+
+export function getSHA256(message: string): string {
+	return CryptoJS.HmacSHA256(message, 'PASSWORD_LHD').toString(CryptoJS.enc.Hex);
+}
+
+export function encrypt(message: string): string {
+	return CryptoJS.AES.encrypt(message, 'PASSWORD_LHD').toString();
+}
+
+export function decrypt(message: string): string {
+	return CryptoJS.AES.decrypt(message, 'PASSWORD_LHD').toString(CryptoJS.enc.Utf8);
+}
