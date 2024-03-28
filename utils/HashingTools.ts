670c754e50b6afcb49f5609fca36f940ce90f9a2
Rosa Maggi <rosa.maggi@epfl.ch>
2024-03-28 12:46:22 +0100

    [Feature] Read secrets from keybase

--- utils/HashingTools.ts
+++ utils/HashingTools.ts
@@ -1,17 +1,21 @@
+import * as dotenv from "dotenv";
+
 const CryptoJS = require("crypto-js");
+dotenv.config();
+const LHD_ENCRYPTION_KEY = process.env.LHD_ENCRYPTION_KEY;
 
 export function generateSalt(): string {
 	return CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex);
 }
 
 export function getSHA256(message: string, salt: string): string {
-	return CryptoJS.HmacSHA256(message + salt, 'PASSWORD_LHD').toString(CryptoJS.enc.Hex);
+	return CryptoJS.HmacSHA256(message + salt, LHD_ENCRYPTION_KEY).toString(CryptoJS.enc.Hex);
 }
 
 export function encrypt(message: string): string {
-	return CryptoJS.AES.encrypt(message, 'PASSWORD_LHD').toString();
+	return CryptoJS.AES.encrypt(message, LHD_ENCRYPTION_KEY).toString();
 }
 
 export function decrypt(message: string): string {
-	return CryptoJS.AES.decrypt(message, 'PASSWORD_LHD').toString(CryptoJS.enc.Utf8);
+	return CryptoJS.AES.decrypt(message, LHD_ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
 }
