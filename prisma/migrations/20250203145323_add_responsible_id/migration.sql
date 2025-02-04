763fa06ea2eaf08b8822047af488b2c8b3ae9f3e
Rosa Maggi <rosa.maggi@epfl.ch>
2025-02-04 23:22:11 +0100

    [feature] add responsible_id into unit model

---
+++ prisma/migrations/20250203145323_add_responsible_id/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `unit` ADD COLUMN `responsible_id` INTEGER NULL;
+
+-- CreateIndex
+CREATE INDEX `responsible_id` ON `unit`(`responsible_id`);
+
+-- AddForeignKey
+ALTER TABLE `unit` ADD CONSTRAINT `unit_has_responsible` FOREIGN KEY (`responsible_id`) REFERENCES `person`(`id_person`) ON DELETE SET NULL ON UPDATE RESTRICT;
