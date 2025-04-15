881f204138049cf57c7cc005ef11f5095175fee5
Rosa Maggi <rosa.maggi@epfl.ch>
2025-04-15 09:31:13 +0200

    [feature] Add modification for organism
    The bio hazard children are modified too

--- schema/bio/bioorg.ts
+++ schema/bio/bioorg.ts
@@ -3,6 +3,9 @@ import {bio_org} from 'nexus-prisma';
 import {createNewMutationLog} from "../global/mutationLogs";
 import {saveBase64File} from "../../utils/File";
 import {mutationStatusType} from "../statuses";
+import {getSHA256} from "../../utils/HashingTools";
+import {id, IDObfuscator} from "../../utils/IDObfuscator";
+import {updateBioOrg} from "../hazards/labHazardChild";
 
 export const BioOrgStruct = objectType({
 	name: bio_org.$name,
@@ -13,8 +16,23 @@ export const BioOrgStruct = objectType({
 		t.field(bio_org.filePath);
 		t.field(bio_org.updated_on);
 		t.field(bio_org.updated_by);
+		t.string('id',  {
+			resolve: async (parent, _, context) => {
+				const encryptedID = IDObfuscator.obfuscate({id: parent.id_bio_org, obj: getBioOrgToString(parent)});
+				return JSON.stringify(encryptedID);
+			},
+		});
 	},
 });
+
+function getBioOrgToString(parent) {
+	return {
+		id: parent.id_bio_org,
+		organism: parent.organism,
+		risk_group: parent.risk_group,
+		filePath: parent.filePath
+	};
+}
 
 export const BioOrgQuery = extendType({
 	type: 'Query',
@@ -46,6 +64,7 @@ export const OrganismsFromFullTextQuery = extendType({
 })
 
 const newOrganismType = {
+	id: stringArg(),
 	organismName: stringArg(),
 	risk: intArg(),
 	fileContent: stringArg(),
@@ -101,6 +120,67 @@ export const OrganismMutations = extendType({
 							await createNewMutationLog(tx, context, tx.bio_org.name, organism.id_bio_org, 'filePath', {}, {'filePath': filePath}, 'CREATE');
 						}
 
+						return mutationStatusType.success();
+					});
+				} catch ( e ) {
+					return mutationStatusType.error(e.message);
+				}
+			}
+		});
+		t.nonNull.field('updateOrganism', {
+			description: `Update organism details.`,
+			args: newOrganismType,
+			type: "OrganismStatus",
+			async resolve(root, args, context) {
+				try {
+					if (context.user.groups.indexOf("LHD_acces_lecture") == -1 && context.user.groups.indexOf("LHD_acces_admin") == -1){
+						throw new Error(`Permission denied`);
+					}
+
+					return await context.prisma.$transaction(async (tx) => {
+						if (!args.id) {
+							throw new Error(`Not allowed to update room`);
+						}
+						const id: id = JSON.parse(args.id);
+						if(id == undefined || id.eph_id == undefined || id.eph_id == '' || id.salt == undefined || id.salt == '') {
+							throw new Error(`Not allowed to update organism`);
+						}
+
+						if(!IDObfuscator.checkSalt(id)) {
+							throw new Error(`Bad descrypted request`);
+						}
+						const idDeobfuscated = IDObfuscator.deobfuscateId(id);
+						const org = await tx.bio_org.findUnique({where: {id_bio_org: idDeobfuscated}});
+						if (! org) {
+							throw new Error(`Organism ${args.organismName} not found.`);
+						}
+						const organismObject =  getSHA256(JSON.stringify(getBioOrgToString(org)), id.salt);
+						if (IDObfuscator.getDataSHA256(id) !== organismObject) {
+							throw new Error(`Organism ${args.organismName} has been changed from another user. Please reload the page to make modifications`);
+						}
+
+						let filePath = org.filePath;
+						if (args.fileContent != '' && args.fileName != '') {
+							filePath = saveBase64File(args.fileContent,  'd_bio/' + org.id_bio_org + '/', args.fileName)
+						}
+
+						const updatedOrganism = await tx.bio_org.update(
+							{ where: { id_bio_org: org.id_bio_org },
+								data: {
+									organism: args.organismName,
+									risk_group: args.risk,
+									updated_on: new Date(),
+									updated_by: context.user.preferred_username,
+									filePath: filePath
+								}
+							});
+
+						if (!updatedOrganism) {
+							throw new Error(`Organism ${args.organismName} not updated.`);
+						} else {
+							await createNewMutationLog(tx, context, tx.bio_org.name, updatedOrganism.id_bio_org, '', org, updatedOrganism, 'UPDATE');
+							await updateBioOrg(org, updatedOrganism, tx, context);
+						}
 						return mutationStatusType.success();
 					});
 				} catch ( e ) {
