bdfc0cc54cdc7749c55fe0727870ef2d3acb2061
Rosa Maggi <rosa.maggi@epfl.ch>
2025-11-26 09:28:58 +0100

    [fix] Merge the if and for constructs

--- model/authorization.ts
+++ model/authorization.ts
@@ -142,128 +142,120 @@ export async function getTheAuthorization(args, prisma) {
 }
 
 async function checkRelations(tx, args, authorization) {
-	if (args.holders) {
-		for ( const holder of args.holders ) {
-			if ( holder.status == 'New' ) {
-				let p = await tx.Person.findUnique({where: {sciper: holder.sciper}});
+	for ( const holder of args.holders || []) {
+		if ( holder.status == 'New' ) {
+			let p = await tx.Person.findUnique({where: {sciper: holder.sciper}});
 
-				if ( !p ) {
-					const ldapUsers = await getUsersFromApi(holder.sciper + "");
-					const ldapUser = ldapUsers["persons"].find(p => p.id == holder.sciper + "");
+			if ( !p ) {
+				const ldapUsers = await getUsersFromApi(holder.sciper + "");
+				const ldapUser = ldapUsers["persons"].find(p => p.id == holder.sciper + "");
 
-					p = await tx.Person.create({
-						data: {
-							surname: ldapUser.lastname,
-							name: ldapUser.firstname,
-							email: ldapUser.email,
-							sciper: parseInt(ldapUser.id)
-						}
-					});
-				}
-
-				const relation = {
-					id_person: Number(p.id_person),
-					id_authorization: Number(authorization.id_authorization)
-				};
-				await tx.authorization_has_holder.create({
-					data: relation
+				p = await tx.Person.create({
+					data: {
+						surname: ldapUser.lastname,
+						name: ldapUser.firstname,
+						email: ldapUser.email,
+						sciper: parseInt(ldapUser.id)
+					}
 				});
-			} else if ( holder.status == 'Deleted' ) {
-				let p = await tx.Person.findUnique({where: {sciper: holder.sciper}});
-				if ( p ) {
-					const whereCondition = {
-						id_authorization: authorization.id_authorization,
-						id_person: p.id_person
-					};
-					await tx.authorization_has_holder.deleteMany({
-						where: whereCondition
-					});
-				}
 			}
-		}
-	}
 
-	if (args.rooms) {
-		for ( const room of args.rooms ) {
-			if ( room.status == 'New' ) {
-				let r = undefined;
-				if ( room.name ) {
-					r = await tx.Room.findFirst({where: {name: room.name, isDeleted: false}})
-				} else if ( room.id ) {
-					r = await tx.Room.findUnique({where: {id: room.id, isDeleted: false}})
-				}
-				if ( !r ) throw new Error(`Authorization not created: room not found`);
-				const relation = {
-					id_lab: Number(r.id),
-					id_authorization: Number(authorization.id_authorization)
-				};
-				await tx.authorization_has_room.create({
-					data: relation
-				});
-			} else if ( room.status == 'Deleted' ) {
-				let p = await tx.Room.findFirst({where: {name: room.name}});
-				if ( p ) {
-					const whereCondition = {
-						id_authorization: authorization.id_authorization,
-						id_lab: p.id
-					};
-					await tx.authorization_has_room.deleteMany({
-						where: whereCondition
-					});
-				}
-			}
-		}
-	}
-
-	if (args.radiations) {
-		for ( const source of args.radiations ) {
-			if ( source.status == 'New' ) {
-				const relation = {
-					id_authorization: Number(authorization.id_authorization),
-					source: source.name
-				};
-				await tx.authorization_has_radiation.create({
-					data: relation
-				});
-			} else if ( source.status == 'Deleted' ) {
+			const relation = {
+				id_person: Number(p.id_person),
+				id_authorization: Number(authorization.id_authorization)
+			};
+			await tx.authorization_has_holder.create({
+				data: relation
+			});
+		} else if ( holder.status == 'Deleted' ) {
+			let p = await tx.Person.findUnique({where: {sciper: holder.sciper}});
+			if ( p ) {
 				const whereCondition = {
 					id_authorization: authorization.id_authorization,
-					source: source.name
+					id_person: p.id_person
 				};
-				await tx.authorization_has_radiation.deleteMany({
+				await tx.authorization_has_holder.deleteMany({
 					where: whereCondition
 				});
 			}
 		}
 	}
 
-	if (args.cas) {
-		for ( const cas of args.cas ) {
-			if ( cas.status == 'New' ) {
-				let p = await tx.auth_chem.findUnique({where: {cas_auth_chem: cas.name}});
-
-				if ( !p ) {
-					throw new Error(`CAS ${cas.name} not found`);
-				}
-
-				const relation = {
-					id_chemical: Number(p.id_auth_chem),
-					id_authorization: Number(authorization.id_authorization)
+	for ( const room of args.rooms || []) {
+		if ( room.status == 'New' ) {
+			let r = undefined;
+			if ( room.name ) {
+				r = await tx.Room.findFirst({where: {name: room.name, isDeleted: false}})
+			} else if ( room.id ) {
+				r = await tx.Room.findUnique({where: {id: room.id, isDeleted: false}})
+			}
+			if ( !r ) throw new Error(`Authorization not created: room not found`);
+			const relation = {
+				id_lab: Number(r.id),
+				id_authorization: Number(authorization.id_authorization)
+			};
+			await tx.authorization_has_room.create({
+				data: relation
+			});
+		} else if ( room.status == 'Deleted' ) {
+			let p = await tx.Room.findFirst({where: {name: room.name}});
+			if ( p ) {
+				const whereCondition = {
+					id_authorization: authorization.id_authorization,
+					id_lab: p.id
 				};
-				await tx.authorization_has_chemical.create({
-					data: relation
+				await tx.authorization_has_room.deleteMany({
+					where: whereCondition
 				});
-			} else if ( cas.status == 'Deleted' ) {
-				let p = await tx.auth_chem.findUnique({where: {cas_auth_chem: cas.name}});
-				if (p) {
-					const whereCondition = {
-						id_authorization: authorization.id_authorization,
-						id_chemical: p.id_auth_chem
-					};
-					await tx.authorization_has_chemical.deleteMany({
-						where: whereCondition
-					});
-				}
+			}
+		}
+	}
+
+	for ( const source of args.radiations || []) {
+		if ( source.status == 'New' ) {
+			const relation = {
+				id_authorization: Number(authorization.id_authorization),
+				source: source.name
+			};
+			await tx.authorization_has_radiation.create({
+				data: relation
+			});
+		} else if ( source.status == 'Deleted' ) {
+			const whereCondition = {
+				id_authorization: authorization.id_authorization,
+				source: source.name
+			};
+			await tx.authorization_has_radiation.deleteMany({
+				where: whereCondition
+			});
+		}
+	}
+
+	for ( const cas of args.cas || []) {
+		if ( cas.status == 'New' ) {
+			let p = await tx.auth_chem.findUnique({where: {cas_auth_chem: cas.name}});
+
+			if ( !p ) {
+				throw new Error(`CAS ${cas.name} not found`);
+			}
+
+			const relation = {
+				id_chemical: Number(p.id_auth_chem),
+				id_authorization: Number(authorization.id_authorization)
+			};
+			await tx.authorization_has_chemical.create({
+				data: relation
+			});
+		} else if ( cas.status == 'Deleted' ) {
+			let p = await tx.auth_chem.findUnique({where: {cas_auth_chem: cas.name}});
+			if (p) {
+				const whereCondition = {
+					id_authorization: authorization.id_authorization,
+					id_chemical: p.id_auth_chem
+				};
+				await tx.authorization_has_chemical.deleteMany({
+					where: whereCondition
+				});
 			}
 		}
 	}
