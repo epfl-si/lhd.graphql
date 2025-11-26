40e1d743620b5e204cce8fe3a7072fa7db67d712
Rosa Maggi <rosa.maggi@epfl.ch>
2025-11-26 13:00:01 +0100

    [fix] Check filename

--- utils/File.ts
+++ utils/File.ts
@@ -5,16 +5,17 @@ dotenv.config();
 const DOCUMENTS_PATH = process.env.DOCUMENTS_PATH;
 
 export function saveBase64File(base64Data: string, filePath: string, fileName: string): string {
-	try {
-		// Remove the data URL part if present
-		const base64Content = base64Data.split(';base64,').pop() || base64Data;
-		// Decode base64 string to buffer
-		const fileBuffer = Buffer.from(base64Content, 'base64');
-		fs.mkdirSync(DOCUMENTS_PATH + "/" + filePath, {recursive: true});
-		// Write the buffer to a file
-		fs.writeFileSync(DOCUMENTS_PATH + "/" + filePath + fileName, fileBuffer);
-		return filePath + fileName;
-	} catch ( e ) {
-		return '';
+	const regex = /^[\p{L}\p{N} _\-\(\)\.]+\.[A-Za-z0-9]+$/u;
+	const validFileName = new RegExp(regex);
+	if (!validFileName.test(fileName)) {
+		throw new Error("File name not permitted");
 	}
+	// Remove the data URL part if present
+	const base64Content = base64Data.split(';base64,').pop() || base64Data;
+	// Decode base64 string to buffer
+	const fileBuffer = Buffer.from(base64Content, 'base64');
+	fs.mkdirSync(DOCUMENTS_PATH + "/" + filePath, {recursive: true});
+	// Write the buffer to a file
+	fs.writeFileSync(DOCUMENTS_PATH + "/" + filePath + fileName, fileBuffer);
+	return filePath + fileName;
 }
