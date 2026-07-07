719f85c9e63ebdfbd54b91bc27ae0670e3bf3afa
Rosa Maggi <rosa.maggi@epfl.ch>
2026-07-07 16:58:00 +0200

    [feature] Add assessments and decisions subject values

---
+++ prisma/migrations/20260707145006_add_assessment_and_decision_values/migration.sql
@@ -0,0 +1,10 @@
+-- This is an empty migration.
+
+INSERT INTO assessment_and_decision_subject (subject) VALUES
+                                                          ('Biological risks'),
+                                                          ('Chemical risks'),
+                                                          ('Noise'),
+                                                          ('Physical risks'),
+                                                          ('Respiratory protection'),
+                                                          ('Ventilation'),
+                                                          ('Other');
