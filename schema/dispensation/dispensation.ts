6b77df8d05194cccdefc380d2bb5c78237a6a361
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-07 17:36:40 +0200

    [fix] `mode: 'insensitive'`

--- schema/dispensation/dispensation.ts
+++ schema/dispensation/dispensation.ts
@@ -240,7 +240,7 @@ export const DispensationsWithPaginationQuery = extendType({
           })
         }
         if (subject) {
-          whereCondition.push({ subject: {is: {subject: {contains: subject}}}})
+          whereCondition.push({ subject: {is: {subject: {contains: subject, mode: 'insensitive'}}}})
         }
         if (ticket) {
           whereCondition.push({ dispensation_has_ticket: { some: {ticket_number: {contains: ticket}} }})
